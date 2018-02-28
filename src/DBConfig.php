a21d6a2315a4703657efda580d26748ee3cd0656
xuantuan <hoang.xuantuan5@gmail.com>
2018-03-01 00:42:39 +0700

    add database query

---
+++ src/DBConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "sql9.freemysqlhosting.net";
+
+//Define your database name here.
+$DatabaseName = "sql9223786";
+
+//Define your database username here.
+$HostUser = "sql9223786";
+
+//Define your database password here.
+$HostPass = "4lD3NFxe2g";
+
+?>
